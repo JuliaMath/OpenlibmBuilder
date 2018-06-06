e885811ab9edd84c94e0834df590324acfb62b6a
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-06 15:00:32 -0700

    Update for `build_tarballs` goodness

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,30 +1,24 @@
 using BinaryBuilder
 
-platforms = [
-  BinaryProvider.Windows(:i686),
-  BinaryProvider.Windows(:x86_64),
-  BinaryProvider.Linux(:i686, :glibc),
-  BinaryProvider.Linux(:x86_64, :glibc),
-  BinaryProvider.Linux(:aarch64, :glibc),
-  BinaryProvider.Linux(:armv7l, :glibc),
-  BinaryProvider.Linux(:powerpc64le, :glibc),
-  BinaryProvider.MacOS()
-]
-
 sources = [
     "https://github.com/JuliaLang/openlibm/archive/v0.5.5.tar.gz" =>
     "07dcc5f59e695fb45167c81406b8e201c5ad91ebf24e3e55ae13298670910cfd",
 ]
 
 script = raw"""
+# Enter the funzone
+cd ${WORKSPACE}/srcdir/openlibm-*
+
 # Install into output
-flags="prefix=${DESTDIR}"
+flags="prefix=${prefix}"
 
 # Build ARCH from ${target}
 flags="${flags} ARCH=${target%-*-*}"
 
-# Enter the funzone
-cd ${WORKSPACE}/srcdir/openlibm-0.5.5
+# Openlibm build system doesn't recognize our windows cross compilers properly
+if [[ ${target} == *mingw* ]]; then
+    flags="${flags} OS=WINNT"
+fi
 
 # Build the library
 make ${flags} -j${nproc}
@@ -33,16 +27,13 @@ make ${flags} -j${nproc}
 make ${flags} install
 """
 
+platforms = supported_platforms()
+
 products = prefix -> [
-    LibraryProduct(prefix, "libopenlibm")
+    LibraryProduct(prefix, "libopenlibm", :libopenlibm)
 ]
 
-# Choose which platforms to build for; if we've got an argument use that one,
-# otherwise default to just building all of them!
-build_platforms = platforms
-if length(ARGS) > 0
-    build_platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(build_platforms), ", "))")
+dependencies = [
+]
 
-autobuild(pwd(), "openlibm", build_platforms, sources, script, products)
+build_tarballs(ARGS, "Openlibm", sources, script, platforms, products, dependencies)
